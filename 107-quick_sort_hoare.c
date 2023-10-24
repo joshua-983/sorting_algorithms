6e56c8882e01f8cb40ac52a1d9bcdaf22ce5336d
joshua-983 <officialgh983@gmail.com>
2023-10-24 06:53:52 -0700

    quick sort

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -3,12 +3,13 @@
 #include <stdlib.h>
 
 /**
- * partition - Finds the partition for the quicksort using the Hoare scheme.
- * @array: Array to sort.
- * @lo: Lowest index partition
- * @hi: Highest index partion
- * @size: Array size
- * Return: Index partition.
+ * partition - finds the partition for the quicksort.
+ * @array: array to sort
+ * @lo: lowest partition to sort
+ * @hi: highest index of the partition to sort
+ * @size: size of the array
+ *
+ * Return: index of the partition
  */
 size_t partition(int *array, ssize_t lo, ssize_t hi, size_t size)
 {
@@ -17,31 +18,36 @@ size_t partition(int *array, ssize_t lo, ssize_t hi, size_t size)
 	pivot = array[hi];
 	while (lo <= hi)
 	{
+		/* Find the elements that need to be swapped */
 		while (array[lo] < pivot)
 			lo++;
 		while (array[hi] > pivot)
 			hi--;
+
 		if (lo <= hi)
 		{
 			if (lo != hi)
 			{
+				/* Swap the elements */
 				swap = array[lo];
 				array[lo] = array[hi];
 				array[hi] = swap;
-				print_array(array, size); /* Print the array after each swap.*/
+
+				/* Print the array after swapping */
+				print_array(array, size);
 			}
 			lo++;
 			hi--;
 		}
 	}
-	return (hi);
+	return (hi); /* Return the index of the partition */
 }
 
 /**
- * quicksort - Sorts a partition of an array int.
- * @array: Array
- * @lo: Lowest index partition
- * @hi: Highest index partition
+ * quicksort - sorts a partition of an array of integers
+ * @array: array to sort
+ * @lo: lowest partition to sort
+ * @hi: highest partition to sort
  * @size: Array size
  *
  * Return: void
@@ -52,16 +58,20 @@ void quicksort(int *array, ssize_t lo, ssize_t hi, size_t size)
 
 	if (lo < hi)
 	{
+		/* Find the pivot index using the partition function */
 		pivot = partition(array, lo, hi, size);
+
+		/* Recursively sort the sub-arrays before and after the pivot */
 		quicksort(array, lo, pivot, size);
 		quicksort(array, pivot + 1, hi, size);
 	}
 }
 
 /**
- * quick_sort_hoare - AN array int to be sorted
- * @array: The array to sort.
+ * quick_sort_hoare - sorts an array of integers in ascending order.
+ * @array: The array to sort
  * @size: Array size
+ *
  * Return: void
  */
 void quick_sort_hoare(int *array, size_t size)
@@ -69,7 +79,7 @@ void quick_sort_hoare(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	/*Start the quicksort process with the entire array.*/
+	/* Call the quicksort function to sort the entire array */
 	quicksort(array, 0, size - 1, size);
 }
 
